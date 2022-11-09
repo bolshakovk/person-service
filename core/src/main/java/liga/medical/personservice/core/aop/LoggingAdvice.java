153615de506c88446afa7332b41f102bf0796245
Kostya <bolshakovk1234@gmail.com>
2022-11-09 17:14:07 +0300

    missing security

--- core/src/main/java/liga/medical/personservice/core/aop/LoggingAdvice.java
+++ core/src/main/java/liga/medical/personservice/core/aop/LoggingAdvice.java
@@ -2,52 +2,45 @@ package liga.medical.personservice.core.aop;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import liga.medical.personservice.core.entity.PersonData;
-import liga.medical.personservice.core.repository.PersonDataRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import model.LogDto;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import service.LogService;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
-@Aspect
 @Slf4j
+@Aspect
 @Component
 @RequiredArgsConstructor
 public class LoggingAdvice {
-
-    private static long idLog;
-    private final PersonDataRepository personDataRepository;
+    private final LogService logService;
     @Pointcut(value = "execution(* liga.medical.personservice.core.controller.*.*(..))")
-    private void generalPointCut(){
+    private void generalPointCut(){}
 
-    }
     @Around(value = "generalPointCut()")
-    public Object applicationLogger(ProceedingJoinPoint proceedingJoinPoint) throws JsonProcessingException{
-        //PersonData personData = personDataRepository.findByName("firstnameuser");
-        //List<PersonData> personDataList = personDataRepository.findAll();
+    public Object applicationLogger(ProceedingJoinPoint proceedingJoinPoint) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
+        String className = proceedingJoinPoint.getTarget().getClass().toString();
         String methodName = proceedingJoinPoint.getSignature().getName();
-        String className = proceedingJoinPoint.getTarget().getClass().getName();
-
-        log.info("invoke class : " + className + " method : " + methodName
-                + "\ndate and time : " + DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now())
-                +"\nid log: " + ++idLog);
-                //+"\nusername: " + personDataList);
+        Object[] array = proceedingJoinPoint.getArgs();
+        log.info("method has been invoked : " + className + ": with args: " + methodName
+                +" in service: " + proceedingJoinPoint.getTarget() + mapper.writeValueAsString(array));
 
         Object obj = null;
         try {
             obj = proceedingJoinPoint.proceed();
-        }catch (Throwable e){
+            logService.addLogsInDBDebug((LogDto) obj);
+        } catch (Throwable e) {
+            log.error(e.getMessage());
+            logService.addLogsInDBExeption((LogDto) obj);
             e.printStackTrace();
         }
-        log.info(className + " with method " + methodName + " response: " + mapper.writeValueAsString(obj));
-        return  obj;
+        log.info(className + ":" + methodName + "() " + "Response: " + mapper.writeValueAsString(obj));
+        return obj;
     }
 }
